93437591cb53aaceb7557cb9c1b759ac8f48ce51
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-09-21 14:04:31 -0600

    Using less constants

--- config/config.js
+++ config/config.js
@@ -2,26 +2,20 @@ const path = require('path');
 
 const DEVELOPMENT = 'development';
 const PRODUCTION = 'production';
-const BUILD_DIRECTORY = 'build';
-const PUBLIC_DIRECTORY = 'public';
-const SOURCE_DIRECTORY = 'src';
 
 module.exports = {
   port: 8080,
-  buildDirectory: BUILD_DIRECTORY,
-  buildPath: '' + path.resolve(BUILD_DIRECTORY),
-  publicBuildDirectory: PUBLIC_DIRECTORY,
-  publicBuildPath: '' + path.resolve(BUILD_DIRECTORY, PUBLIC_DIRECTORY),
+  buildPath: '' + path.resolve('build'),
+  publicBuildPath: '' + path.resolve('build', 'public'),
   serverOutputName: 'server.js',
   clientOutputName: 'client.js',
-  serverEntryFile: '' + path.resolve(SOURCE_DIRECTORY, 'server', 'server.js'),
-  clientEntryFile: '' + path.resolve(SOURCE_DIRECTORY, 'client', 'client.js'),
-  htmlEntryFile: '' + path.resolve(SOURCE_DIRECTORY, 'client', 'index.html'),
+  serverEntryFile: '' + path.resolve('src', 'server', 'server.js'),
+  clientEntryFile: '' + path.resolve('src', 'client', 'client.js'),
+  htmlEntryFile: '' + path.resolve('src', 'client', 'index.html'),
   publicPath: '/',
   devtool: 'cheap-module-eval-source-map',
   pollMs: 500,
   development: DEVELOPMENT,
   production: PRODUCTION,
   environment: DEVELOPMENT
-  // environment: PRODUCTION
 };
