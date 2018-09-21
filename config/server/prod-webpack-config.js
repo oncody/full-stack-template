4e6bf1bc65b2072c0beee6dc66cf6e5bcdbcff87
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-09-20 19:50:47 -0600

    Removing unneeded comments and config

--- config/server/prod-webpack-config.js
+++ config/server/prod-webpack-config.js
@@ -3,10 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 const config = {
   externals: [
     nodeExternals()
-  ],
-  node: {
-    __dirname: false
-  }
+  ]
 };
 
 module.exports = config;
--- server/routes/cold-public-route.js
+++ server/routes/cold-public-route.js
@@ -5,9 +5,6 @@ const projectConfig = require('../../config/config');
 const router = express.Router();
 
 if (projectConfig.environment !== projectConfig.development) {
-  // todo: see if this works
-  // router.use(express.static(path.join(__dirname, 'public')));
-  // router.use(express.static(path.resolve(projectConfig.publicBuildDirectory)));
   router.use(express.static(projectConfig.publicBuildPath));
 }
 
