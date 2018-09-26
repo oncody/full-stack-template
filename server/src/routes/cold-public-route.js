c1970c4534b3ebaf2eec3d009846f1b6c9a2ddd6
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-09-26 07:31:21 -0600

    Removing old commented out code

--- server/src/routes/cold-public-route.js
+++ server/src/routes/cold-public-route.js
@@ -7,8 +7,6 @@ const router = express.Router();
 // eslint-disable-next-line no-process-env
 if (process.env.NODE_ENV === projectConfig.productionEnvironment) {
   router.use(projectConfig.publicRoute, express.static(projectConfig.productionWebOutputPath));
-  // todo: remove this line below
-  // router.use(express.static(webConfig.outputDirectory));
 }
 
 module.exports = router;
